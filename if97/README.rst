385764bb8952c9bb119d73d00be7b6b0b2edb0f5
Aaron Lentner <aaron.d.lentner@gmail.com>
2017-03-29 12:31:32 -0400

    make working hello world test

---
+++ if97/README.rst
@@ -0,0 +1,11 @@
+A Water Properties Python Project
+=======================
+
+A Python package for providing water and steam properties based on the 2007
+revised release of the Industial FormuLation 1997 from the 
+International Assotiation for the Properties of Water and Steam.
+
+----
+
+[ Insert descriptions of the following: overview of the project, basic
+  usage examples, etc.]
---
+++ if97/setup.py
@@ -0,0 +1,34 @@
+"""A setuptools based setup module for the if97 Python package
+
+"""
+from setuptools import setup
+
+# Get the long description from the README file
+with open('README.rst') as file:
+
+    long_description = file.read()
+
+# Define the setup configuration
+setup(
+    name = 'if97',
+    version = '1.0.1',
+    description = 'A Python package for providing water properties', 
+    long_description = long_description,
+    url = 'https://github.com/alentner/if97',
+    author = 'Aaron Lentner',
+    author_email = 'aaron.d.lentner@gmail.com',
+    license = 'MIT',
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Physics :: Material Properties',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only',
+        ],
+    keywords = 'water steam properties industrial formulation 97 IAPWS',
+    packages=['if97'],
+    install_requirments = [ ],
+    entry_points = { },
+    include_package_data = True,
+    zip_safe = False
+    )
