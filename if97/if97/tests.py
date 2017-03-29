8ed4f15f16fe453cc6e9014886448522b5ae60bc
Aaron Lentner <aaron.d.lentner@gmail.com>
2017-03-29 15:45:45 -0400

    add cv tests to region 1

--- if97/if97/tests.py
+++ if97/if97/tests.py
@@ -8,6 +8,7 @@ class test_ThermodynamicProperty_Region1(unittest.TestCase):
         self.assertEqual(round(region1.u(3, 300), 6), 0.112324818E3, 'Failed specific internal energy, state 1, region 1!')
         self.assertEqual(round(region1.s(3, 300), 9), 0.392294792, 'Failed specific entropy, state 1, region 1!')
         self.assertEqual(round(region1.cp(3, 300), 8), 0.417301218E1, 'Failed specific heat capacity, state 1, region 1!')
+        self.assertEqual(round(region1.cv(3, 300), 8), 0.412120160E1, 'Failed specific heat capacity, state 1, region 1!')
         self.assertEqual(round(region1.w(3, 300), 5), 0.150773921E4, 'Failed speed of sound, state 1, region 1!')
     def test_ThermodynamicProperty_Region1_state2(self):
         self.assertEqual(round(region1.v(80, 300), 12), 0.971180894E-3, 'Failed specific volume, state 1, region 1!')
@@ -15,6 +16,7 @@ class test_ThermodynamicProperty_Region1(unittest.TestCase):
         self.assertEqual(round(region1.u(80, 300), 6), 0.106448356E3, 'Failed specific internal energy, state 1, region 1!')
         self.assertEqual(round(region1.s(80, 300), 9), 0.368563852, 'Failed specific entropy, state 1, region 1!')
         self.assertEqual(round(region1.cp(80, 300), 8), 0.401008987E1, 'Failed specific heat capacity, state 1, region 1!')
+        self.assertEqual(round(region1.cv(80, 300), 8), 0.391736606E1, 'Failed specific heat capacity, state 1, region 1!')
         self.assertEqual(round(region1.w(80, 300), 5), 0.163469054E4, 'Failed speed of sound, state 1, region 1!')
     def test_ThermodynamicProperty_Region1_state3(self):
         self.assertEqual(round(region1.v(3, 500), 11), 0.120241800E-2, 'Failed specific volume, state 1, region 1!')
@@ -22,6 +24,7 @@ class test_ThermodynamicProperty_Region1(unittest.TestCase):
         self.assertEqual(round(region1.u(3, 500), 6), 0.971934985E3, 'Failed specific internal energy, state 1, region 1!')
         self.assertEqual(round(region1.s(3, 500), 8), 0.258041912E1, 'Failed specific entropy, state 1, region 1!')
         self.assertEqual(round(region1.cp(3, 500), 8), 0.465580682E1, 'Failed specific heat capacity, state 1, region 1!')
+        self.assertEqual(round(region1.cv(3, 500), 8), 0.322139223E1, 'Failed specific heat capacity, state 1, region 1!')
         self.assertEqual(round(region1.w(3, 500), 5), 0.124071337E4, 'Failed speed of sound, state 1, region 1!')
 if __name__ == '__main__':
     unittest.main()
