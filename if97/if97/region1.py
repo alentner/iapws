6b863a9f1fc680f3309cfb88bb26811e00eda10a
Aaron Lentner <aaron.d.lentner@gmail.com>
2017-03-29 15:18:48 -0400

    initial functional commit of region 1

--- if97/if97/region1.py
+++ if97/if97/region1.py
@@ -1,5 +1,94 @@
-def sayhello():
-    print('Hello World!')
+# Constants for region 1
+I = [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 8, 8, 21, 23, 29, 30, 31, 32]
+J = [-2, -1, 0, 1, 2, 3, 4, 5, -9, -7, -1, 0, 1, 3, -3, 0, 1, 3, 17, -4, 0, 6, -5, -2, 10, -8, -11, -6, -29, -31, -38, -39, -40, -41]
+n = [0.14632971213167, -0.84548187169114, -0.37563603672040E1, 0.33855169168385E1, -0.95791963387872, 0.15772038513228, -0.16616417199501E-1,  0.81214629983568E-3, 0.28319080123804E-3, -0.60706301565874E-3, -0.18990068218419E-1,
+     -0.32529748770505E-1, -0.21841717175414E-1, -0.52838357969930E-4, -0.47184321073267E-3, -0.30001780793026E-3, 0.47661393906987E-4, -0.44141845330846E-5, -0.72694996297594E-15, -0.31679644845054E-4, -0.28270797985312E-5,
+     -0.85205128120103E-9, -0.22425281908000E-5, -0.65171222895601E-6, -0.14341729937924E-12, -0.40516996860117E-6, -0.12734301741641E-8, -0.17424871230634E-9, -0.68762131295531E-18, 0.14478307828521E-19, 0.26335781662795E-22,
+     -0.11947622640071E-22, 0.18228094581404E-23, -0.93537087292458E-25]
 
-if __name__ == '__main__':
-    sayhello()
+def gamma(pi, tau):
+    sum = 0
+    for Ii, Ji, ni in zip(I, J, n):
+        sum += ni * (7.1 - pi)**Ii * (tau - 1.222)**Ji
+    return sum
+def gamma_pi(pi, tau):
+    sum = 0
+    for Ii, Ji, ni in zip(I, J, n):
+        sum += -ni * Ii * (7.1 - pi)**(Ii - 1) * (tau - 1.222)**Ji
+    return sum
+def gamma_pipi(pi, tau):
+    sum = 0
+    for Ii, Ji, ni in zip(I, J, n):
+        sum += ni * Ii * (Ii - 1) * (7.1 - pi)**(Ii - 2) * (tau - 1.222)**Ji
+    return sum
+def gamma_tau(pi, tau):
+    sum = 0
+    for Ii, Ji, ni in zip(I, J, n):
+        sum += ni * (7.1 - pi)**Ii * Ji * (tau - 1.222)**(Ji - 1)
+    return sum
+def gamma_tautau(pi, tau):
+    sum = 0
+    for Ii, Ji, ni in zip(I, J, n):
+        sum += ni * (7.1 - pi)**Ii * Ji * (Ji - 1) * (tau - 1.222)**(Ji - 2)
+    return sum
+def gamma_pitau(pi, tau):
+    sum = 0
+    for Ii, Ji, ni in zip(I, J, n):
+        sum += -ni * Ii * (7.1 - pi)**(Ii - 1) * Ji * (tau - 1.222)**(Ji - 1)
+    return sum
+def gibbs(P, T):
+    pi = P / 16.53
+    tau = 1386 / T
+    R = 0.461526
+
+    return gamma(pi, tau) * R * T
+def v(P, T):
+    pi = P / 16.53
+    tau = 1386 / T
+    R = 0.461526
+
+    return pi * gamma_pi(pi, tau) * R * T / (P * 10**6 / 1000)
+def h(P, T):
+    pi = P / 16.53
+    tau = 1386 / T
+    R = 0.461526
+
+    return tau * gamma_tau(pi, tau) * R * T
+def u(P, T):
+    pi = P / 16.53
+    tau = 1386 / T
+    R = 0.461526
+
+    return (tau * gamma_tau(pi, tau) - pi * gamma_pi(pi, tau)) * R * T    
+def s(P, T):
+    pi = P / 16.53
+    tau = 1386 / T
+    R = 0.461526
+
+    return (tau * gamma_tau(pi, tau) - gamma(pi, tau)) * R
+def cp(P, T):
+    pi = P / 16.53
+    tau = 1386 / T
+    R = 0.461526
+
+    return -tau**2 * gamma_tautau(pi, tau) * R
+def cv(P, T):
+    pi = P / 16.53
+    tau = 1386 / T
+    R = 0.461526
+
+    return (-tau**2 * gamma_tautau(pi, tau) + (gamma_pi(pi, tau) - tau * gamma_pitau(pi, tau))**2 / gamma_pipi(pi, tau)) * R
+def w(P, T):
+    pi = P / 16.53
+    tau = 1386 / T
+    R = 0.461526
+
+    return (gamma_pi(pi, tau)**2 * R * T * 1000 / ((gamma_pi(pi, tau) - tau * gamma_pitau(pi, tau))**2 / (tau**2 * gamma_tautau(pi, tau)) - gamma_pipi(pi, tau)))**0.5
+
+# Temporary project entry point
+#def sayhello():
+#    print('Hello World!')
+#    print(v(3, 300))
+
+#if __name__ == '__main__':
+#    sayhello()
---
+++ if97/if97/tests.py
@@ -0,0 +1,27 @@
+import unittest
+from if97 import region1
+
+class Test_ThermodynamicProperty_Region1(unittest.TestCase):
+    def test_ThermodynamicProperty_Region1_state1(self):
+        self.assertEqual(round(region1.v(3, 300), 11), 0.100215168E-2, 'Failed specific volume, state 1, region 1!')
+        self.assertEqual(round(region1.h(3, 300), 6), 0.115331273E3, 'Failed specific enthalpy, state 1, region 1!')
+        self.assertEqual(round(region1.u(3, 300), 6), 0.112324818E3, 'Failed specific internal energy, state 1, region 1!')
+        self.assertEqual(round(region1.s(3, 300), 9), 0.392294792, 'Failed specific entropy, state 1, region 1!')
+        self.assertEqual(round(region1.cp(3, 300), 8), 0.417301218E1, 'Failed specific heat capacity, state 1, region 1!')
+        self.assertEqual(round(region1.w(3, 300), 5), 0.150773921E4, 'Failed speed of sound, state 1, region 1!')
+    def test_ThermodynamicProperty_Region1_state2(self):
+        self.assertEqual(round(region1.v(80, 300), 12), 0.971180894E-3, 'Failed specific volume, state 1, region 1!')
+        self.assertEqual(round(region1.h(80, 300), 6), 0.184142828E3, 'Failed specific enthalpy, state 1, region 1!')
+        self.assertEqual(round(region1.u(80, 300), 6), 0.106448356E3, 'Failed specific internal energy, state 1, region 1!')
+        self.assertEqual(round(region1.s(80, 300), 9), 0.368563852, 'Failed specific entropy, state 1, region 1!')
+        self.assertEqual(round(region1.cp(80, 300), 8), 0.401008987E1, 'Failed specific heat capacity, state 1, region 1!')
+        self.assertEqual(round(region1.w(80, 300), 5), 0.163469054E4, 'Failed speed of sound, state 1, region 1!')
+    def test_ThermodynamicProperty_Region1_state3(self):
+        self.assertEqual(round(region1.v(3, 500), 11), 0.120241800E-2, 'Failed specific volume, state 1, region 1!')
+        self.assertEqual(round(region1.h(3, 500), 6), 0.975542239E3, 'Failed specific enthalpy, state 1, region 1!')
+        self.assertEqual(round(region1.u(3, 500), 6), 0.971934985E3, 'Failed specific internal energy, state 1, region 1!')
+        self.assertEqual(round(region1.s(3, 500), 8), 0.258041912E1, 'Failed specific entropy, state 1, region 1!')
+        self.assertEqual(round(region1.cp(3, 500), 8), 0.465580682E1, 'Failed specific heat capacity, state 1, region 1!')
+        self.assertEqual(round(region1.w(3, 500), 5), 0.124071337E4, 'Failed speed of sound, state 1, region 1!')
+if __name__ == '__main__':
+    unittest.main()
